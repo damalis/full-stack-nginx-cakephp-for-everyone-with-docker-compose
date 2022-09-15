a6939f718901007b6f65b7796328278fc7b5e440
Erdal ALTIN <erdalaltin@gmail.com>
2022-09-15 19:20:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ clear
 echo ""
 echo "======================================================================="
 echo "|                                                                     |"
-echo "|   full-stack-nginx-cakephp-for-everyone-with-docker-compose   |"
+echo "|     full-stack-nginx-cakephp-for-everyone-with-docker-compose       |"
 echo "|                         by Erdal ALTIN                              |"
 echo "|                                                                     |"
 echo "======================================================================="
